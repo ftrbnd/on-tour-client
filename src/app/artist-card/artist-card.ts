ade020f053dccf57b5c23fad363fed72e0ac55fa
ftrbnd <giosalas25@gmail.com>
2025-11-06 14:56:23 -0800

    Create artist-card component

--- src/app/artist-card/artist-card.ts
+++ src/app/artist-card/artist-card.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+import { ArtistData } from './artist-data';
+import { CardModule } from 'primeng/card';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-artist-card',
-  imports: [],
+  imports: [CardModule, ButtonModule],
   templateUrl: './artist-card.html',
   styleUrl: './artist-card.css',
 })
-export class ArtistCard {}
+export class ArtistCard {
+  artist = input<ArtistData>();
+}
--- src/app/artists-page/artists-page.ts
+++ src/app/artists-page/artists-page.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { ArtistData } from '../artist-card/artist-data';
 import { ArtistsService } from '../artists-service';
+import { ArtistCard } from '../artist-card/artist-card';
 
 @Component({
   selector: 'app-artists-page',
-  imports: [],
+  imports: [ArtistCard],
   templateUrl: './artists-page.html',
   styleUrl: './artists-page.css',
 })
@@ -14,6 +15,9 @@ export class ArtistsPage implements OnInit {
   artists = signal<ArtistData[]>([]);
 
   ngOnInit(): void {
-    this.artistsService.getArtists().subscribe((res) => this.artists.set(res));
+    this.artistsService.getArtists().subscribe((res) => {
+      console.log(res);
+      this.artists.set(res);
+    });
   }
 }
