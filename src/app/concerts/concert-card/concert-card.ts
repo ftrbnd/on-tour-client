f17876e6df7f893018221ff09909b61827a36830
ftrbnd <giosalas25@gmail.com>
2025-11-13 12:17:39 -0800

    Add concert details to card

--- src/app/concerts/concert-card/concert-card.ts
+++ src/app/concerts/concert-card/concert-card.ts
@@ -4,10 +4,11 @@ import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
 import { Message } from 'primeng/message';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-concert-card',
-  imports: [CardModule, ButtonModule, RouterLink, Message],
+  imports: [CardModule, ButtonModule, RouterLink, Message, AvatarModule],
   templateUrl: './concert-card.html',
 })
 export class ConcertCard {
