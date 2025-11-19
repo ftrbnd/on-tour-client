29a4de2160b086f1b99ffb7bdf9ce0aa15b89257
ftrbnd <giosalas25@gmail.com>
2025-11-19 15:37:07 -0800

    Add badge indicator to venue cards

--- src/app/venues/venue-card/venue-card.ts
+++ src/app/venues/venue-card/venue-card.ts
@@ -3,10 +3,11 @@ import { VenueData } from '../venue-data';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
 
 @Component({
   selector: 'app-venue-card',
-  imports: [CardModule, ButtonModule, RouterLink],
+  imports: [CardModule, ButtonModule, RouterLink, OverlayBadgeModule],
   templateUrl: './venue-card.html',
 })
 export class VenueCard {
