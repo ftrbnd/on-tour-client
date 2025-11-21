35f14bd97142f16c5e1b00d838c33e1e1c2a7f9e
ftrbnd <giosalas25@gmail.com>
2025-11-21 13:43:57 -0800

    Add concerts badge to artist cards

--- src/app/artists/artist-card/artist-card.ts
+++ src/app/artists/artist-card/artist-card.ts
@@ -3,10 +3,11 @@ import { ArtistData } from '../artist-data';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
 
 @Component({
   selector: 'app-artist-card',
-  imports: [CardModule, ButtonModule, RouterLink],
+  imports: [CardModule, ButtonModule, RouterLink, OverlayBadgeModule],
   templateUrl: './artist-card.html',
 })
 export class ArtistCard {
