e642417c1cefc1b7f388e88f220d131ff1a423d6
ftrbnd <giosalas25@gmail.com>
2025-11-27 12:32:21 -0800

    Add more details to concert-log component

--- src/app/concert-logs/concert-log/concert-log.ts
+++ src/app/concert-logs/concert-log/concert-log.ts
@@ -1,13 +1,15 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { ConcertLogData } from '../concert-log-data';
 import { CardModule } from 'primeng/card';
 import { RatingModule } from 'primeng/rating';
 import { FormsModule } from '@angular/forms';
 import { ConcertData } from '../../concerts/concert-data';
+import { DatePipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-concert-log',
-  imports: [CardModule, RatingModule, FormsModule],
+  imports: [CardModule, RatingModule, FormsModule, DatePipe, RouterLink],
   templateUrl: './concert-log.html',
 })
 export class ConcertLog {
