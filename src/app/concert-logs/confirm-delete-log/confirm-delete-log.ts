9d9728971c4375b4470586eacacc5154d70e6b4a
ftrbnd <giosalas25@gmail.com>
2025-12-15 22:39:51 -0800

    Navigate after deleting

--- src/app/concert-logs/confirm-delete-log/confirm-delete-log.ts
+++ src/app/concert-logs/confirm-delete-log/confirm-delete-log.ts
@@ -3,6 +3,7 @@ import { ConcertLogsService } from '../concert-logs-service';
 import { ConcertLogData } from '../concert-log-data';
 import { Dialog } from 'primeng/dialog';
 import { ButtonModule } from 'primeng/button';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-confirm-delete-log',
@@ -11,6 +12,7 @@ import { ButtonModule } from 'primeng/button';
 })
 export class ConfirmDeleteLog {
   concertLogsService = inject(ConcertLogsService);
+  router = inject(Router);
 
   concertLog = input.required<ConcertLogData>();
   visible = model.required<boolean>();
@@ -18,7 +20,13 @@ export class ConfirmDeleteLog {
   handleDelete() {
     this.concertLogsService.deleteConcertLog(this.concertLog().id).subscribe({
       error: (err) => console.error(err),
-      next: () => window.location.reload(),
+      next: () => {
+        if (this.router.url.includes('/logs')) {
+          this.router.navigateByUrl(`/concerts/${this.concertLog().concertId}`);
+        } else {
+          window.location.reload();
+        }
+      },
     });
   }
 }
